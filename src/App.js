f44feae4d6ca0b7e0761c10b2dae147328303675
Blaze-012 <besty255a@gmail.com>
2023-11-09 12:10:23 +0530

    layout is aadded

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import EntryPage from './pages/Entry/Entry.page';
+import { DefaultLayout } from './Layout/DefaultLayout';
 
 function App() {
   return (
     <div className="App">
-      <EntryPage />
+      {/* <EntryPage /> */}
+      <DefaultLayout>
+        //Dashboard
+      </DefaultLayout>
     </div>
   );
 }
---
+++ src/Layout/DefaultLayout.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Header } from './partials/Header.comp.js';
+import { Footer } from './partials/Footer.comp.js';
+
+export const DefaultLayout = ({children}) => {
+    return (
+        <div className='default-layout'>
+            <header className='header'><Header /></header>
+            <main className='main'>
+                {children}
+            </main>
+            <footer className='footer'><Footer /></footer>
+            
+        </div>
+    );
+};
